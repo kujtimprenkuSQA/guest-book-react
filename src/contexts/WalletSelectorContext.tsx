58af2934d77a5b7d87805ec4f2851f1a97a6425e
Kujtim Prenku <kujtim.prenku@sqa-consulting.com>
2023-06-26 11:00:02 +0200

    Add imports for all wallets commented for now.

--- src/contexts/WalletSelectorContext.tsx
+++ src/contexts/WalletSelectorContext.tsx
@@ -1,15 +1,29 @@
 import React, { useCallback, useContext, useEffect, useState } from "react";
 import { map, distinctUntilChanged } from "rxjs";
+
 import { setupWalletSelector } from "@near-wallet-selector/core";
 import type { WalletSelector, AccountState } from "@near-wallet-selector/core";
 import { setupModal } from "@near-wallet-selector/modal-ui";
 import type { WalletSelectorModal } from "@near-wallet-selector/modal-ui";
-import { setupNearWallet } from "@near-wallet-selector/near-wallet";
-import { setupSender } from "@near-wallet-selector/sender";
-import { setupMathWallet } from "@near-wallet-selector/math-wallet";
-import { setupLedger } from "@near-wallet-selector/ledger";
-// @ts-ignore
+
+// import { setupCoin98Wallet } from "@near-wallet-selector/coin98-wallet";
+// import { setupFinerWallet } from "@near-wallet-selector/finer-wallet";
+// import { setupHereWallet } from "@near-wallet-selector/here-wallet";
+// import { setupLedger } from "@near-wallet-selector/ledger";
+// import { setupMathWallet } from "@near-wallet-selector/math-wallet";
+// import { setupMeteorWallet } from "@near-wallet-selector/meteor-wallet";
+// import { setupMyNearWallet } from "@near-wallet-selector/my-near-wallet";
+// import { setupNearWallet } from "@near-wallet-selector/near-wallet";
+// import { setupNarwallets } from "@near-wallet-selector/narwallets";
+// import { setupNearFi } from "@near-wallet-selector/nearfi";
+// import { setupNeth } from "@near-wallet-selector/neth";
+// import { setupNightly } from "@near-wallet-selector/nightly";
+// import { setupNightlyConnect } from "@near-wallet-selector/nightly-connect";
+// import { setupOptoWallet } from "@near-wallet-selector/opto-wallet";
+// import { setupSender } from "@near-wallet-selector/sender";
 // import { setupWalletConnect } from "@near-wallet-selector/wallet-connect";
+// import { setupWelldoneWallet } from "@near-wallet-selector/welldone-wallet";
+// import { setupXDEFI } from "@near-wallet-selector/xdefi";
 import { CONTRACT_ID } from "../constants";
 
 declare global {
@@ -67,10 +81,31 @@ export const WalletSelectorContextProvider: React.FC<{
       network: "testnet",
       debug: true,
       modules: [
-        setupNearWallet(),
-        setupSender(),
-        setupMathWallet(),
-        setupLedger(),
+        // setupMyNearWallet(),
+        // setupCoin98Wallet(),
+        // setupFinerWallet(),
+        // setupHereWallet(),
+        // setupLedger(),
+        // setupMathWallet(),
+        // setupMeteorWallet(),
+        // setupNearWallet(),
+        // setupNarwallets(),
+        // setupNearFi(),
+        // setupNeth({
+        //   bundle: false
+        // }),
+        // setupNightly(),
+        // setupNightlyConnect({
+        //   url: "wss://relay.nightly.app/app",
+        //   appMetadata: {
+        //     additionalInfo: "",
+        //     application: "NEAR Wallet Selector",
+        //     description: "Example dApp used by NEAR Wallet Selector",
+        //     icon: "https://near.org/wp-content/uploads/2020/09/cropped-favicon-192x192.png",
+        //   },
+        // }),
+        // setupOptoWallet(),
+        // setupSender(),
         // setupWalletConnect({
         //   projectId: "c8cb6204543639c31aef44ea4837a554",
         //   metadata: {
@@ -80,9 +115,9 @@ export const WalletSelectorContextProvider: React.FC<{
         //     icons: ["https://avatars.githubusercontent.com/u/37784886"],
         //   },
         // }),
+        // setupWelldoneWallet(),
+        // setupXDEFI(),
       ],
-
-
     });
 
     const _modal = setupModal(_selector, { contractId: CONTRACT_ID });
@@ -111,6 +146,7 @@ export const WalletSelectorContextProvider: React.FC<{
 
     const subscription = selector.store.observable
       .pipe(
+        // @ts-ignore
         map((state) => state.accounts),
         distinctUntilChanged()
       )
